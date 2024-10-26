23d8e29db519d0c214309254b1d8bef7687df4b0
sarrus1811 <142220801+sarrus1811@users.noreply.github.com>
2024-10-26 15:58:15 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -19,12 +19,9 @@ function admin_tools(){
 function dev_tools(){
 	echo "Setting up developer tools..."
 	sleep 1
-	apt install emacs build-essential git golang python3 wget gpg gdb valgrind -y
-	
-	# Installs nvm (Node Version Manager)
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+	apt install emacs curl wget build-essential git golang ruby-full python3 wget gpg gdb valgrind -y
      
-	#Install VScode
+	# Install VScode
 	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -32,6 +29,23 @@ function dev_tools(){
 	apt install apt-transport-https
 	apt update
 	apt install code # or code-insiders
+
+ 	# Install Docker
+  	# Add Docker's official GPG key:
+	sudo apt-get update
+	sudo apt-get install ca-certificates curl
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+	
+	# Add the repository to Apt sources:
+	echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update
+ 	# Finally install Docker
+  	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 		
 	# Install VScode extensions
 	echo "Installing VScode extensions"
